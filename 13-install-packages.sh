7eace80e3c994ef2423069217ff9508a2059e8e4
unknown <rajeshveeranki478@gmail.com>
2024-02-06 19:17:54 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -7,16 +7,16 @@ Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/TMP/$0-$TIMESTAMP.LOG"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-       echo -e "$2...$R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
     else
-        echo -e "$2...$G SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -34,15 +34,12 @@ fi # fi means reverse of if, indicating condition end
 
 for package in $@
 do
-   yum list installed $package &>> $LOGFILE #check installed are not
-   if [ $? -ne 0 ] #if not installed
-   then
-       yum install $package -y &>> $LOGFILE # installed package
-       VALIDATE $? "installation of $package" # validate
+    yum list installed $package &>> $LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y &>> $LOGFILE # install the package
+        VALIDATE $? "Installation of $package" # validate
     else
-        echo -e "$package is allready installed ... $Y SKIPPING $N"
+        echo -e "$package is already installed ... $Y SKIPPING $N"
     fi
-done    
-
-
- 
+done
