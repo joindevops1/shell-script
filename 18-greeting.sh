8d0005aa0adceca041365fc7173a2bb53b8cf73b
unknown <rajeshveeranki478@gmail.com>
2024-02-12 17:24:36 +0530

    daws-76s

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "Hello $PERSON, Good morning.we are learing shell-script"
+USAGE(){
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
+    echo "options::"
+    echo " -n, specify the name (mandatory)"
+    echo " -w, specify the wishes.ex, Good morning"
+    echo " -h, Display help and exit"
+}
+
+while getopt ":n:w:h" opt; do
+    case $opt in
+         n)NAME="$OPTARG";;
+         w)WISHES="$OPTARG";;
+         h|*)USER; exit;;
+    esac
+done    
