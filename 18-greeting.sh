8f50f500208f6e0f4b609b78699e199b78f9e5fe
unknown <rajeshveeranki478@gmail.com>
2024-02-12 21:25:38 +0530

    daws-76s

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -21,6 +21,7 @@ while getopts ":n:w:h" opt; do
     esac
 done
 
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
 if [ -z "$NAME" ]; then # now wishes is optional
     #echo "ERROR: Both -n and -w are mandatory options."
     echo "ERROR: -n is mandatory."
